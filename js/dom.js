f790e3f77b3809c7747536a80d9a9c6fecc4cbe2
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-07-10 13:28:45 +0600

    task-1 done

--- js/dom.js
+++ js/dom.js
@@ -1,6 +1,6 @@
 console.log("I am here form DOM");
 
 const allHeadings2 = document.querySelectorAll("h2");
-for (allHeadings of allHeadings2) {
-  allHeadings.style.color = "orange";
+for (headings of allHeadings2) {
+  headings.style.color = "lightblue";
 }
