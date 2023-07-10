942f7061cf4c7a86436edf15fae395de2175a91f
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-07-10 13:35:30 +0600

    task-1, 2, 32 done

--- js/dom.js
+++ js/dom.js
@@ -4,3 +4,6 @@ const allHeadings2 = document.querySelectorAll("h2");
 for (headings of allHeadings2) {
   headings.style.color = "lightblue";
 }
+
+const backpackContainer = document.getElementById("backpack");
+backpackContainer.style.backgroundColor = "tomato";
