e3f693611dfb29bb0459a676f678e9bff9b174cc
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-07-10 13:42:00 +0600

    task-1, 2, 3, 4 done

--- js/dom.js
+++ js/dom.js
@@ -1,9 +1,18 @@
+// task-1
 console.log("I am here form DOM");
 
+// task-2
 const allHeadings2 = document.querySelectorAll("h2");
 for (headings of allHeadings2) {
   headings.style.color = "lightblue";
 }
 
+// task-3
 const backpackContainer = document.getElementById("backpack");
 backpackContainer.style.backgroundColor = "tomato";
+
+// task-4
+const cardsClass = document.getElementsByClassName("card");
+for (cards of cardsClass) {
+  cards.style.borderRadius = "30px";
+}
