14bcdafb7306a406580c60c13fed48608fb5bacb
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-07-10 19:25:13 +0600

    task-1, 2, 3, 4, 5, 6, done

--- js/dom.js
+++ js/dom.js
@@ -32,3 +32,31 @@ buttonBuy2.addEventListener("click", function () {
   const buyNowCard2 = document.getElementById("card-2");
   buyNowCard2.style.display = "none";
 });
+
+const buttonBuy3 = document.getElementById("button-buy-3");
+buttonBuy3.addEventListener("click", function () {
+  // task-6
+  const buyNowCard3 = document.getElementById("card-3");
+  buyNowCard3.style.display = "none";
+});
+
+const buttonBuy4 = document.getElementById("button-buy-4");
+buttonBuy4.addEventListener("click", function () {
+  // task-6
+  const buyNowCard4 = document.getElementById("card-4");
+  buyNowCard4.style.display = "none";
+});
+
+const buttonBuy5 = document.getElementById("button-buy-5");
+buttonBuy5.addEventListener("click", function () {
+  // task-6
+  const buyNowCard5 = document.getElementById("card-5");
+  buyNowCard5.style.display = "none";
+});
+
+const buttonBuy6 = document.getElementById("button-buy-6");
+buttonBuy6.addEventListener("click", function () {
+  // task-6
+  const buyNowCard6 = document.getElementById("card-6");
+  buyNowCard6.style.display = "none";
+});
